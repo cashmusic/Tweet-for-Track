1c1b4c65d3ad75b0f13e468a25dea92eba6d9a95
Jesse von Doom <jesse@cashmusic.org>
2010-04-06 16:55:08 -0400

    stripped google code

--- content_download.php
+++ content_download.php
@@ -53,14 +53,5 @@ window.addEvent('domready', function() {
 	</div> 
 </div> 
  
-<script type="text/javascript"> 
-//<![CDATA[ 
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-//]]> 
-</script><script type="text/javascript"> 
-//<![CDATA[ 
-try {var pageTracker = _gat._getTracker("UA-7451645-1");pageTracker._trackPageview();} catch(err) {}
-//]]> 
-</script> 
 </body> 
 </html>
